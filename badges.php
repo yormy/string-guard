98ab44e78176e82527c8e1bce67777f28995da7b
Rob <rob@email.com>
2024-04-06 13:13:35 +0200

    json not found

--- badges.php
+++ badges.php
@@ -2,7 +2,7 @@
 
 echo 'badges';
 
-$path = 'phpinsights.json';
+$path = './badges/phpinsights.json';
 if (!file_exists($path))
 {
     exec("wget https://img.shields.io/badge/test_badge_coverage-missing-red -O badges/coverage.svg");
@@ -12,6 +12,9 @@ if (!file_exists($path))
 $jsonString = file_get_contents($path);
 $jsonData = json_decode($jsonString);
 
-$codePercentage = $jsonData->summary->code;
+$codePercentage = (int)$jsonData?->summary?->code;
+if (!$codePercentage) {
+    $codePercentage = 1;
+}
 
 exec("wget https://img.shields.io/badge/test_badge_coverage-$codePercentage%25-blue -O badges/coverage.svg");
