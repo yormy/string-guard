99f9f3a1f02af73c89be8fb2eb072c9cfc6f673b
Rob <rob@email.com>
2024-04-06 12:20:24 +0200

    wip final workflow

--- badges.php
+++ badges.php
@@ -2,8 +2,10 @@
 
 echo 'badges';
 
-$path = 'test.json';
+$path = 'phpinsights.json';
 $jsonString = file_get_contents($path);
 $jsonData = json_decode($jsonString);
 
-echo $jsonData->summary->code;
+$codePercentage = $jsonData->summary->code;
+
+exec("wget https://img.shields.io/badge/test_badge_coverage-$codePercentage%25-blue -O badges/coverage.svg");
