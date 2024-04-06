780d75313b92ef4a79fad04c095cb71d47d6f2f2
Rob <rob@email.com>
2024-04-06 21:51:38 +0200

    a2- working3

--- badges.php
+++ badges.php
@@ -7,8 +7,6 @@ if (!file_exists($path))
 {
 //    $file = 'badges/debug1.txt';
 //    file_put_contents($file, getcwd(), FILE_APPEND | LOCK_EX);
-
-
     exec("wget https://img.shields.io/badge/test_badge_coverage-missing-red -O badges/coverage.svg");
     return;
 }
