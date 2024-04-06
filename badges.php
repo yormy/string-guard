273044e09c6cad5b59957238feb2b82ef1f32bb6
Rob <rob@email.com>
2024-04-06 21:58:43 +0200

    a2- working3

--- badges.php
+++ badges.php
@@ -1,13 +1,15 @@
 <?php
-
-echo 'badges';
-
 $path = './badges/phpinsights.json';
+$coverageText = 'coverage';
+
+$scoreLow = 30;
+$scoreMedium = 60;
+
 if (!file_exists($path))
 {
 //    $file = 'badges/debug1.txt';
 //    file_put_contents($file, getcwd(), FILE_APPEND | LOCK_EX);
-    exec("wget https://img.shields.io/badge/test_badge_coverage-missing-red -O badges/coverage.svg");
+    exec("wget https://img.shields.io/badge/$coverageText-missing-red -O badges/coverage.svg");
     return;
 }
 
@@ -15,8 +17,12 @@ $jsonString = file_get_contents($path);
 $jsonData = json_decode($jsonString);
 
 $codePercentage = (int)$jsonData?->summary?->code;
-if (!$codePercentage) {
-    $codePercentage = 1;
+if ($codePercentage < $scoreLow) {
+    $color = 'red';
+} elseif ($codePercentage < $scoreMedium) {
+    $color = 'orange';
+} else {
+    $color = 'green';
 }
 
-exec("wget https://img.shields.io/badge/test_badge_coverage-$codePercentage%25-blue -O badges/coverage.svg");
+exec("wget https://img.shields.io/badge/$coverageText-$codePercentage%25-$color -O badges/coverage.svg");
